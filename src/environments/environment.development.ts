089235fbee633d4e66645e5e50b6cf6617ef5eca
aparzero30 <jimcin30@gmail.com>
2024-12-05 08:10:35 +0800

    Added proxy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  backendDomain: "https://54.253.2.130"
+  // backendDomain: "https://54.253.2.130"
   // backendDomain: "http://localhost:8080",
+  backendDomain: "https://proxy-server-three-delta.vercel.app"
 };
