8bd14f4d9ce30e41c0fad10f68ec95bc9a87e964
aparzero30 <jimcin30@gmail.com>
2024-12-09 10:07:48 +0800

    revert

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // backendDomain: "https://proxy-server-three-delta.vercel.app",
-  backendDomain: "https://api.aparzero.store",
+  backendDomain: "https://proxy-server-three-delta.vercel.app",
+  // backendDomain: "https://api.aparzero.store",
 };
