7a2265d00d2950c0ee555977a2b824a20da491ad
aparzero30 <jimcin30@gmail.com>
2024-12-09 09:59:09 +0800

    change backend to new domain

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  backendDomain: "https://proxy-server-three-delta.vercel.app",
+  // backendDomain: "https://proxy-server-three-delta.vercel.app",
+  backendDomain: "https://api.aparzero.store",
 };
